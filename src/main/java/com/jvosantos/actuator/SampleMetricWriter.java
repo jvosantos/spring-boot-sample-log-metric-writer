c398c583c8370681befe64634b53ee99e0531de3
Vasco Santos <vasco.santos@mindera.com>
2017-07-06 16:51:10 +0100

    Loggers instead of system out println

--- src/main/java/com/jvosantos/actuator/SampleMetricWriter.java
+++ src/main/java/com/jvosantos/actuator/SampleMetricWriter.java
@@ -1,23 +1,26 @@
 package com.jvosantos.actuator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.actuate.metrics.Metric;
 import org.springframework.boot.actuate.metrics.writer.Delta;
 import org.springframework.boot.actuate.metrics.writer.MetricWriter;
 
 public class SampleMetricWriter implements MetricWriter {
+    public static final Logger LOGGER = LoggerFactory.getLogger(SampleMetricWriter.class);
+
     @Override
     public void increment(Delta<?> delta) {
-        System.out.println(delta.getName() + " - " + delta.getValue());
+        LOGGER.info("increment: " + delta.getName() + " - " + delta.getValue());
     }
 
     @Override
     public void reset(String s) {
-        System.out.println(s);
-
+        LOGGER.info("reset: " + s);
     }
 
     @Override
     public void set(Metric<?> metric) {
-        System.out.println(metric.getName() + " - " + metric.getValue());
+        LOGGER.info("set: " + metric.getName() + " - " + metric.getValue());
     }
 }
